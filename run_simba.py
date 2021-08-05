f97c38f984af0e624d4949b55f98d16139e00942
Sarthak Trivedi <sarthak.trivedi33@gmail.com>
2021-08-05 18:45:01 +0200

    Add files via upload

--- run_simba.py
+++ run_simba.py
@@ -5,19 +5,25 @@ from tensorflow.keras.applications import ResNet50,ResNet101,ResNet152,ResNet50V
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+import cv2
 
 parser = argparse.ArgumentParser(description='Runs SimBA on a set of images')
 parser.add_argument('--data_root', type=str, required=True, help='root directory of imagenet data')
 parser.add_argument('--result_dir', type=str, default='save', help='directory for saving results')
 parser.add_argument('--model', type=str, default='resnet50', help='type of base model to use')
+parser.add_argument('--attack_mode', type=str, default='dct', help='type of base model to use')
+parser.add_argument('--output', type=str, default='save', help='number of image samples')
 parser.add_argument('--num_runs', type=int, default=1000, help='number of image samples')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size for parallel runs')
-parser.add_argument('--num_iters', type=int, default=10000, help='maximum number of iterations')
 parser.add_argument('--log_every', type=int, default=10, help='log every n iterations')
 parser.add_argument('--epsilon', type=float, default=0.2, help='step size per iteration')
 parser.add_argument('--save_suffix', type=str, default='', help='suffix appended to save file')
 args = parser.parse_args()
-
+if not os.path.isdir(os.path.join('.',args.output)):
+    os.mkdir(args.output)
+    os.mkdir(os.path.join(args.output,'original'))
+    os.mkdir(os.path.join(args.output,'modified'))
 models = {'resnet50':ResNet50(weights='imagenet'),
           'resnet101':ResNet101(weights='imagenet'),
           'resnet151':ResNet152(weights='imagenet'),
@@ -46,9 +52,11 @@ query_log = np.zeros(shape=len(loader),dtype=np.float)
 l2_norm_log = np.zeros(shape=len(loader),dtype=np.float)
 for i in range(len(loader)):
     x,y = next(loader)
-    x_,succ,query,l2_norm = sim.simba_single(x,y,iters=args.num_runs,epsilon=args.epsilon,attack_mode='dct',log_every=args.log_every)
+    x_,succ,query,l2_norm = sim.simba_single(x,y,iters=args.num_runs,epsilon=args.epsilon,attack_mode=args.attack_mode,log_every=args.log_every)
     print(succ,query,l2_norm)
     succ_log[i],query_log[i],l2_norm_log[i] = succ, query, l2_norm
-    plt.imshow(x_/255)
-    plt.draw()
-    plt.pause(0.1)
+    plt.imsave(os.path.join('.',args.output,'original',f'{i}.jpg'),x /255)
+    plt.imsave(os.path.join('.', args.output, 'modified', f'{i}.jpg'), x_ / 255)
+with open(os.path.join('.',args.output,'data.json'),'w') as f:
+    f.write(json.dumps({'succ_log':succ_log.tolist(),'query_log':query_log.tolist(),'l2_norm_log':l2_norm_log.tolist()}))
+    f.close()
